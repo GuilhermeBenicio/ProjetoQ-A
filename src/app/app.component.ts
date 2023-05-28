b9ebc4a8a4a1c56ad4d8e5a63adba2e5a849f3fe
Guilherme Benicio <guilhermebenicio18102003@gmail.com>
2023-05-28 13:14:22 -0300

    primeiro commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'projeto_BD';
+  profileImageUrl: string = '';
+  personName: string = '';
+  personAge: number = 0;
+  personPhone: string = '';
+  personEmail: string = '';
+
+  constructor(private http: HttpClient) {}
+  ngOnInit(): void {
+    // Aqui você pode fazer chamadas HTTP para a API
+    this.http.get('https://randomuser.me/api/').subscribe((data: any) => {
+      const user = data.results[0];
+      const firstName = user.name.first;
+      const lastName = user.name.last;
+      this.personName = `${firstName} ${lastName}`;
+
+      this.personAge = user.dob.age;
+      this.personPhone = user.phone;
+      this.personEmail = user.email;
+    });
+
+    this.http
+      .get('https://dog.ceo/api/breeds/image/random')
+      .subscribe((data: any) => {
+        console.log(data);
+        const teste = data.message;
+        this.profileImageUrl = teste;
+      });
+  }
 }
